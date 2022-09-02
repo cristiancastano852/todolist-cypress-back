60ce5287f063b2be700c87147c98b32b5b72e65c
valeria <valeria.granada@evertecinc.com>
2022-09-01 22:17:33 -0500

    feat: implement todo list

---
+++ index.js
@@ -0,0 +1,12 @@
+const tasks = require('./routes/tasks')
+const cors = require('cors');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/tasks', tasks);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Listening on port ${port}...`))
